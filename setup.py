70e821c4688d84ca7d96bf80b0366b6807210388
Felix Sonntag <felix@cobrainer.com>
2018-09-12 10:09:33 +0200

    Loosened dependency versions

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(
     packages=find_packages(exclude='test'),  # same as name
     license='MIT',
     install_requires=[
-        'torch==0.4.0',
-        'awscli==1.14.32',
-        'gensim==3.4.0',
-        'typing==3.6.4',
-        'tqdm==4.23.4',
-        'segtok==1.5.6'
+        'torch>=0.4.0,<0.5.0',
+        'awscli>=1.14.32',
+        'gensim>=3.4.0',
+        'typing>=3.6.4',
+        'tqdm>=4.23.4',
+        'segtok>=1.5.6'
     ],
     include_package_data=True,
     python_requires='>=3.6',
